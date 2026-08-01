993055a5accb1a2da8703b061955fed7dc4ba923
Magnus Ihse Bursie <mag@icus.se>
2026-08-01 21:42:19 +0200

    chore: Expose config from main Mod class

--- src/main/java/se/icus/mag/loomassistant/LoomAssistantMod.java
+++ src/main/java/se/icus/mag/loomassistant/LoomAssistantMod.java
@@ -20,6 +20,10 @@ public class LoomAssistantMod implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static LoomAssistantConfig getConfig() {
+        return AutoConfig.getConfigHolder(LoomAssistantConfig.class).getConfig();
+    }
+
     @Override
     public void onInitialize() {
         LoomAssistantMod.LOGGER.info("Initializing LoomAssistantMod");
